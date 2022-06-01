7968e47791847b0f26b4f7193205a4256ddfee8f
Zornitsa Gencheva <zornitsa.sertova@gmail.com>
2022-06-01 22:53:46 +0300

    P03_FootballBetting

---
+++ EntityRelations/EntityRelations/P03_FootballBetting/P03_FootballBetting/Data/Models/Game.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace P03_FootballBetting.Data.Models
+{
+    public class Game
+    {
+        public Game()
+        {
+            this.PlayerStatistics = new HashSet<PlayerStatistic>();
+            this.Bets = new HashSet<Bet>();
+        }
+        public int GameId { get; set; }
+        [Required]
+
+        [ForeignKey("HomeTeam")]
+        public int HomeTeamId { get; set; }
+        [InverseProperty(nameof(Team.HomeGames))]
+        public Team HomeTeam { get; set; }
+        [Required]
+        [ForeignKey("AwayTeam")]
+        public int AwayTeamId { get; set; }
+        [InverseProperty(nameof(Team.AwayGames))]
+        public Team AwayTeam { get; set; }
+
+        public int HomeTeamGoals { get; set; }
+        public int AwayTeamGoals { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public double HomeTeamBetRate { get; set; }
+        public double AwayTeamBetRate { get; set; }
+        public double DrawBetRate { get; set; }
+
+        public string Result { get; set; }
+
+       
+        public ICollection<PlayerStatistic> PlayerStatistics { get; set; }
+
+        public ICollection<Bet> Bets { get; set; }
+
+
+    }
+}
---
+++ EntityRelations/EntityRelations/P03_FootballBetting/P03_FootballBetting/Migrations/20220601121031_InitialCreate.cs
@@ -0,0 +1,326 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace P03_FootballBetting.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Color",
+                columns: table => new
+                {
+                    ColorId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Color", x => x.ColorId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Country",
+                columns: table => new
+                {
+                    CountryId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Country", x => x.CountryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Position",
+                columns: table => new
+                {
+                    PositionId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Position", x => x.PositionId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(nullable: false),
+                    Email = table.Column<string>(nullable: false),
+                    Password = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Balance = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Town",
+                columns: table => new
+                {
+                    TownId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    CountryId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Town", x => x.TownId);
+                    table.ForeignKey(
+                        name: "FK_Town_Country_CountryId",
+                        column: x => x.CountryId,
+                        principalTable: "Country",
+                        principalColumn: "CountryId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Team",
+                columns: table => new
+                {
+                    TeamId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    LogoUrl = table.Column<string>(type: "varchar(2048)", nullable: true),
+                    Initials = table.Column<string>(type: "char(3)", nullable: true),
+                    Budget = table.Column<decimal>(nullable: false),
+                    PrimaryKitColorId = table.Column<int>(nullable: false),
+                    SecondaryKitColorId = table.Column<int>(nullable: false),
+                    TownId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Team", x => x.TeamId);
+                    table.ForeignKey(
+                        name: "FK_Team_Color_PrimaryKitColorId",
+                        column: x => x.PrimaryKitColorId,
+                        principalTable: "Color",
+                        principalColumn: "ColorId");
+                    table.ForeignKey(
+                        name: "FK_Team_Color_SecondaryKitColorId",
+                        column: x => x.SecondaryKitColorId,
+                        principalTable: "Color",
+                        principalColumn: "ColorId");
+                    table.ForeignKey(
+                        name: "FK_Team_Town_TownId",
+                        column: x => x.TownId,
+                        principalTable: "Town",
+                        principalColumn: "TownId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Game",
+                columns: table => new
+                {
+                    GameId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    HomeTeamId = table.Column<int>(nullable: false),
+                    AwayTeamId = table.Column<int>(nullable: false),
+                    HomeTeamGoals = table.Column<int>(nullable: false),
+                    AwayTeamGoals = table.Column<int>(nullable: false),
+                    DateTime = table.Column<DateTime>(nullable: false),
+                    HomeTeamBetRate = table.Column<double>(nullable: false),
+                    AwayTeamBetRate = table.Column<double>(nullable: false),
+                    DrawBetRate = table.Column<double>(nullable: false),
+                    Result = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Game", x => x.GameId);
+                    table.ForeignKey(
+                        name: "FK_Game_Team_AwayTeamId",
+                        column: x => x.AwayTeamId,
+                        principalTable: "Team",
+                        principalColumn: "TeamId");
+                    table.ForeignKey(
+                        name: "FK_Game_Team_HomeTeamId",
+                        column: x => x.HomeTeamId,
+                        principalTable: "Team",
+                        principalColumn: "TeamId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Player",
+                columns: table => new
+                {
+                    PlayerId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    SquadNumber = table.Column<int>(nullable: false),
+                    TeamId = table.Column<int>(nullable: false),
+                    PositionId = table.Column<int>(nullable: false),
+                    IsInjured = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Player", x => x.PlayerId);
+                    table.ForeignKey(
+                        name: "FK_Player_Position_PositionId",
+                        column: x => x.PositionId,
+                        principalTable: "Position",
+                        principalColumn: "PositionId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Player_Team_TeamId",
+                        column: x => x.TeamId,
+                        principalTable: "Team",
+                        principalColumn: "TeamId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Bet",
+                columns: table => new
+                {
+                    BetId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Amount = table.Column<decimal>(nullable: false),
+                    Prediction = table.Column<string>(nullable: false),
+                    DateTime = table.Column<DateTime>(nullable: false),
+                    UserId = table.Column<int>(nullable: false),
+                    GameId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bet", x => x.BetId);
+                    table.ForeignKey(
+                        name: "FK_Bet_Game_GameId",
+                        column: x => x.GameId,
+                        principalTable: "Game",
+                        principalColumn: "GameId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Bet_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PlayerStatistic",
+                columns: table => new
+                {
+                    GameId = table.Column<int>(nullable: false),
+                    PlayerId = table.Column<int>(nullable: false),
+                    ScoredGoals = table.Column<int>(nullable: false),
+                    Assists = table.Column<int>(nullable: false),
+                    MinutesPlayed = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlayerStatistic", x => new { x.PlayerId, x.GameId });
+                    table.ForeignKey(
+                        name: "FK_PlayerStatistic_Game_GameId",
+                        column: x => x.GameId,
+                        principalTable: "Game",
+                        principalColumn: "GameId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PlayerStatistic_Player_PlayerId",
+                        column: x => x.PlayerId,
+                        principalTable: "Player",
+                        principalColumn: "PlayerId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bet_GameId",
+                table: "Bet",
+                column: "GameId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bet_UserId",
+                table: "Bet",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Game_AwayTeamId",
+                table: "Game",
+                column: "AwayTeamId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Game_HomeTeamId",
+                table: "Game",
+                column: "HomeTeamId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Player_PositionId",
+                table: "Player",
+                column: "PositionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Player_TeamId",
+                table: "Player",
+                column: "TeamId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PlayerStatistic_GameId",
+                table: "PlayerStatistic",
+                column: "GameId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Team_PrimaryKitColorId",
+                table: "Team",
+                column: "PrimaryKitColorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Team_SecondaryKitColorId",
+                table: "Team",
+                column: "SecondaryKitColorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Team_TownId",
+                table: "Team",
+                column: "TownId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Town_CountryId",
+                table: "Town",
+                column: "CountryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bet");
+
+            migrationBuilder.DropTable(
+                name: "PlayerStatistic");
+
+            migrationBuilder.DropTable(
+                name: "User");
+
+            migrationBuilder.DropTable(
+                name: "Game");
+
+            migrationBuilder.DropTable(
+                name: "Player");
+
+            migrationBuilder.DropTable(
+                name: "Position");
+
+            migrationBuilder.DropTable(
+                name: "Team");
+
+            migrationBuilder.DropTable(
+                name: "Color");
+
+            migrationBuilder.DropTable(
+                name: "Town");
+
+            migrationBuilder.DropTable(
+                name: "Country");
+        }
+    }
+}
