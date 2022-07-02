a8228b146cadac0c9c23e7bc372517eb321277d4
Zornitsa Gencheva <zornitsa.sertova@gmail.com>
2022-07-02 23:51:25 +0300

    Quiz workshop with IDesignTimeDbContextFactory configuration and first migrations

--- WorkShop/Quiz/Quiz.Data/ApplicationDbContext.cs
+++ WorkShop/Quiz/Quiz.Data/ApplicationDbContext.cs
@@ -1,16 +1,20 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using Quiz.Models;
+
 
 namespace Quiz.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        public DbSet<Models.Quiz> MyProperty { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Answer> Answers { get; set; }
     }
 }
